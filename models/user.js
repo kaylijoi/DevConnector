30587ecda91193765e1ad92f4b41ce8ae2c1d032
kaylijoi <kaylideshon@gmail.com>
2021-03-07 12:24:27 -0800

    build user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create user schema
+// By default, required is false
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Starts building collection
+const User = mongoose.model('users', UserSchema)
+module.exports = User;
