e3bb587891c4133b0977a1ab4147e376be0fe4cd
kaylijoi <kaylideshon@gmail.com>
2021-04-03 23:44:16 -0700

    Add auth component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import NavBar from './components/layouts/Navbar';
+import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Footer from './components/layouts/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component = {Register} />
+          <Route exact path="/login" component={Login} />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { axios } from 'axios';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    }
+  }
+
+  onChange(e) {
+    this.setState({[e.target.name]:e.target.value });
+  }
+
+  onSubmit(e) {
+    
+  }
+
+  render() {
+    return (
+      <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Sign Up</h1>
+            <p className="lead text-center">Create your DevConnector account</p>
+            <form onSubmit={this.onSubmit.bind(this)}>
+              <div className="form-group">
+                <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" 
+                value = {this.state.name}
+                onChange={this.onChange.bind(this)}
+                required />
+              </div>
+              <div className="form-group">
+                <input type="email" className="form-control form-control-lg" placeholder="Email Address"
+                value = {this.state.email}
+                onChange={this.onChange.bind(this)}
+                name="email" />
+                <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Password"
+                value = {this.state.password}
+                onChange={this.onChange.bind(this)}
+                name="password" />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" 
+                value = {this.state.password2}
+                onChange={this.onChange.bind(this)}
+                name="password2" />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
+export default Register;
